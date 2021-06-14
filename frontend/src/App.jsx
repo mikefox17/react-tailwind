027c0a9c4a583c3a26e4a9f8c50c0f5b7b04b698
Michael Fox <mikefox17@gmail.com>
2021-06-14 12:56:47 -0400

    sanity/tailwind/vite added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h2 className='text-6xl'> Hey there</h2>
+            <p />
+            <text-3xl className='text-purple-600 4xl'>bitches</text-3xl>
+        </div>
+    );
+};
+
+export default App;
